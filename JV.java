be166fa7dfca46ac66ea834b9e4af4c98c3e4ea4
lucaerichsen <101435916+lucaerichsen@users.noreply.github.com>
2022-09-12 19:05:26 -0300

    Update JV.java

--- JV.java
+++ JV.java
@@ -70,7 +70,7 @@ T[7] = m[0][2] + m[1][1] + m[2][0];
   for(int i=0; i<T.length; i++){
    if(T[i].equals("XXX")){
        vencedor = "Jogador 1";
-   }else if(T[i].equals("000")){
+   }else if(T[i].equals("OOO")){
        vencedor = "Jogador 2";
    }   
   }                       
